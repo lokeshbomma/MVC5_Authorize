8436162a86a9e52d691dbf2d539f40b2aaa64380
lokesh <lokeshbomma349@gmail.com>
2024-07-27 15:33:45 +0530

    Add project files.

---
+++ MVC5_Authorize/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using MVC5_Authorize.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+
+namespace MVC5_Authorize.Controllers
+{
+    
+    public class LoginController : Controller
+    {
+        LoginDBEntities db = new LoginDBEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Models.Table user, string ReturnUrl)
+        {
+            if (IsValid(user))
+            {
+                FormsAuthentication.SetAuthCookie(user.UserName, false);
+                Session["UserName"] = user.UserName.ToString();
+                if (ReturnUrl != null)
+                {
+                    return Redirect(ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session["UserName"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+        public bool IsValid(Models.Table user)
+        {
+            var data = db.Tables.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+            if (data != null)
+            {
+                return (user.UserName == data.UserName && user.Password == data.Password);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
